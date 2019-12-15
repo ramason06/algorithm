0b350d07c2fc96da1f70a06e072ecb1c907a763e
ramason <ddd15113@gmail.com>
2019-12-15 23:41:43 +0900

    lengthOfLongestSubstringTwoDistinct

---
+++ Java/src/LongSubstringTwoDistinct.java
@@ -0,0 +1,31 @@
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class LongSubstringTwoDistinct {
+    public int lengthOfLongestSubstringTwoDistinct(String s) {
+        int n = s.length();
+        if (n < 3) return n;
+
+        int lIdx = 0;
+        int rIdx = 0;
+
+        Map<Character, Integer> hashMap = new HashMap<>();
+
+        int maxx = 2;
+
+        while (rIdx < n) {
+            if (hashMap.size() < 3)
+                hashMap.put(s.charAt(rIdx), rIdx++);
+
+            if (hashMap.size() == 3) {
+                int minIdx = Collections.min(hashMap.values());
+                hashMap.remove(s.charAt(minIdx));
+                lIdx = minIdx + 1;
+            }
+
+            maxx = Math.max(maxx, rIdx - lIdx);
+        }
+        return maxx;
+    }
+}
