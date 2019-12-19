ecca5c1314d36d48e6d98c7f1e02d80ef5cc2a59
ramason <ddd15113@gmail.com>
2019-12-19 21:24:54 +0900

    leetCode - Meeting rooms

---
+++ Java/src/MeetingRooms.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MeetingRooms {
+    public boolean canAttendMeetings(int[][] intervals) {
+        Arrays.sort(intervals, (o1, o2) -> {
+            return o1[0] == o2[0] ? Integer.compare(o1[1], o2[1]) : Integer.compare(o1[0], o2[0]);
+        });
+        new ArrayList();
+
+        for(int i = 0; i < intervals.length - 1; ++i) {
+            if (intervals[i][1] > intervals[i + 1][0]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
