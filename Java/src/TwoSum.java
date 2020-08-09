d40b998373d56bae0c0a5ab95c112c9e37070f6f
ramason <ddd15113@gmail.com>
2020-08-09 19:57:43 +0900

    Two sum

---
+++ Java/src/TwoSum.java
@@ -0,0 +1,34 @@
+package src;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+    public int[] twoSumEasy(int[] nums, int target) {
+        for(int i = 0; i < nums.length; i++) {
+            for(int j = i; j < nums.length; j++) {
+                if(i==j) continue;
+                if(nums[i] + nums[j] == target) {
+                    return new int[]{i,j};
+                }
+            }
+        }
+        return new int[]{-1,-1};
+    }
+
+    public int[] toSumHash(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++) {
+            int com = target - nums[i];
+            if(map.containsKey(com)) {
+                return new int[]{map.get(com), i};
+            }
+            map.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("Wrong");
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
